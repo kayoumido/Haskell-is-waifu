e3b07ac95461743bfd789c8c02b2a806cbe3bc1b
Doran Kayoumi <dorankayoumi@gmail.com>
2020-01-28 13:43:13 +0100

    Rem Tree part. Wrong lab oooops D:

--- Study/Lab5.hs
+++ Study/Lab5.hs
@@ -50,25 +50,3 @@ del a l@(Node b bs)
     | a < b = l
     | a == b = bs
     | otherwise = Node b (del a bs)
-
-
-
-data Tree a = Nil
-    | Nod a (Tree a) (Tree a)
-    deriving(Show, Read)
-
-tins a Nil = Nod a Nil Nil
-tins a t@(Nod x lhs rhs)
-    | a == x = t
-    | a < x = Nod x (tins a lhs) rhs
-    | otherwise = Nod x lhs (tins a rhs)
-
-tsearch _ Nil = False
-tsearch a (Nod x lhs rhs)
-    | a == x = True
-    | a < x = tsearch a lhs
-    | otherwise = tsearch a rhs
-
-squash Nil = []
-squash (Nod a Nil Nil) = [a]
-squash (Nod a lhs rhs) = squash lhs ++ [a] ++ squash rhs
